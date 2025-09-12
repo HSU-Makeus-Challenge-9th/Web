cd36ffe747957287f1b2a8d777daa39913d172a4
dongyeol02 <sprent7@gmail.com>
2025-09-12 16:50:26 +0900

    feat : 0주차-3 미션1완료

---
+++ Week00/dongyeol02/week00-3/mission1/app.js
@@ -0,0 +1,45 @@
+const todoList = document.getElementById("todoList");
+const completeList = document.getElementById("completeList");
+const todoInput = document.getElementById("todoInput");
+
+todoInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    addTodo();
+  }
+});
+
+const addTodo = () => {
+  todoText = todoInput.value.trim();
+
+  // 빈 입력일 경우 예외처리!
+  if (todoText === "") {
+    alert("할 일을 입력해주세요!");
+    return;
+  }
+  // 최대 30글자
+  if (todoText.length > 30) {
+    alert("할 일은 30자 이내로 입력해주세요!");
+    return;
+  }
+  const li = document.createElement("li");
+  li.innerHTML = `<span>${todoText}</span><button onclick="completeTodo(this)">완료</button>`;
+  todoList.appendChild(li);
+  todoInput.value = "";
+  alert("할 일이 추가되었습니다!");
+};
+
+const completeTodo = (button) => {
+  const completeLi = button.parentElement;
+  const li = document.createElement("li");
+  const todoText = completeLi.querySelector("span").innerText;
+  li.innerHTML = `<span>${todoText}</span><button onclick="deleteTodo(this)">삭제</button>`;
+  completeList.appendChild(li);
+  completeLi.remove();
+  alert("해야 할 일이 완료되었습니다!");
+};
+
+const deleteTodo = (button) => {
+  const deleteLi = button.parentElement;
+  deleteLi.remove();
+  alert("한 일이 삭제되었습니다!");
+};
