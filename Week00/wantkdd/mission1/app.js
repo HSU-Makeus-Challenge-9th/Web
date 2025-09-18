8066f46a5bd60ab7c0dcb6df098f0e901f76f5c5
Gamgyul <ryusangwan12@gmail.com>
2025-09-18 23:18:36 +0900

    refactor: 0주차 미션 피드백 반영

--- Week00/wantkdd/mission1/app.js
+++ Week00/wantkdd/mission1/app.js
@@ -1,50 +1,65 @@
-const input = document.getElementById('input');
-const todoList = document.getElementById('todo');
-const doneList = document.getElementById('done');
+const $input = document.getElementById('input');
+const $todoList = document.getElementById('todo');
+const $doneList = document.getElementById('done');
 
-input.addEventListener('keypress', function (e) {
+$input.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
     addTodo();
   }
 });
 
+function createTodo(todo) {
+  const li = document.createElement('li');
+  li.innerHTML = `<span>${todo}</span> <button class="complete">완료</button>`;
+  li.querySelector('.complete').addEventListener('click', () => {
+    completeTodo(todo);
+  });
+  return li;
+}
+
+function createDone(todo) {
+  const li = document.createElement('li');
+  li.innerHTML = `<span class="done">${todo}</span> <button class="delete">삭제</button>`;
+  li.querySelector('.delete').addEventListener('click', () => {
+    deleteTodo(todo);
+  });
+  return li;
+}
+
+function deleteTodo(todo) {
+  const dones = JSON.parse(localStorage.getItem('dones')) || [];
+  const newDones = dones.filter((done) => done !== todo);
+  localStorage.setItem('dones', JSON.stringify(newDones));
+  loadTodos();
+}
+
 function loadTodos() {
   const todos = JSON.parse(localStorage.getItem('todos')) || [];
   const dones = JSON.parse(localStorage.getItem('dones')) || [];
 
-  todoList.innerHTML = '';
-  doneList.innerHTML = '';
+  $todoList.innerHTML = '';
+  $doneList.innerHTML = '';
 
   todos.forEach((todo) => {
-    const li = document.createElement('li');
-    li.innerHTML = `<span>${todo}</span> <button class="complete">완료</button>`;
-    li.querySelector('.complete').addEventListener('click', () =>
-      completeTodo(todo)
-    );
-    todoList.appendChild(li);
+    const todo = createTodo(todo);
+    $todoList.appendChild(todo);
   });
 
   dones.forEach((todo) => {
-    const li = document.createElement('li');
-    li.innerHTML = `<span class="done">${todo}</span> <button class="delete">삭제</button>`;
-    li.querySelector('.delete').addEventListener('click', () => {
-      const newDones = dones.filter((done) => done !== todo);
-      localStorage.setItem('dones', JSON.stringify(newDones));
-      loadTodos();
-    });
-    doneList.appendChild(li);
+    const done = createDone(todo);
+    $doneList.appendChild(done);
   });
 }
 
 function addTodo() {
-  const text = input.value.trim();
+  const text = $input.value.trim();
   if (!text) return;
 
   const todos = JSON.parse(localStorage.getItem('todos')) || [];
   todos.push(text);
   localStorage.setItem('todos', JSON.stringify(todos));
 
-  input.value = '';
+  $input.value = '';
   loadTodos();
 }
 
