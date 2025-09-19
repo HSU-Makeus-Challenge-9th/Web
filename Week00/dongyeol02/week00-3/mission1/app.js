ce095562679ba4194d114ea871c09a129a7c6758
dongyeol02 <sprent7@gmail.com>
2025-09-19 13:00:58 +0900

    fix: css box-sizing 적용 및 변수명 컨벤션 적용

--- Week00/dongyeol02/week00-3/mission1/app.js
+++ Week00/dongyeol02/week00-3/mission1/app.js
@@ -1,15 +1,15 @@
-const todoList = document.getElementById("todoList");
-const completeList = document.getElementById("completeList");
-const todoInput = document.getElementById("todoInput");
+const $todoList = document.getElementById("todoList");
+const $completeList = document.getElementById("completeList");
+const $todoInput = document.getElementById("todoInput");
 
-todoInput.addEventListener("keypress", function (e) {
+$todoInput.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     addTodo();
   }
 });
 
 const addTodo = () => {
-  todoText = todoInput.value.trim();
+  todoText = $todoInput.value.trim();
 
   // 빈 입력일 경우 예외처리!
   if (todoText === "") {
@@ -23,23 +23,23 @@ const addTodo = () => {
   }
   const li = document.createElement("li");
   li.innerHTML = `<span>${todoText}</span><button onclick="completeTodo(this)">완료</button>`;
-  todoList.appendChild(li);
-  todoInput.value = "";
+  $todoList.appendChild(li);
+  $todoInput.value = "";
   alert("할 일이 추가되었습니다!");
 };
 
 const completeTodo = (button) => {
-  const completeLi = button.parentElement;
-  const li = document.createElement("li");
-  const todoText = completeLi.querySelector("span").innerText;
-  li.innerHTML = `<span>${todoText}</span><button onclick="deleteTodo(this)">삭제</button>`;
-  completeList.appendChild(li);
-  completeLi.remove();
+  const $completeItem = button.parentElement;
+  const $li = document.createElement("li");
+  const todoText = $completeItem.querySelector("span").innerText;
+  $li.innerHTML = `<span>${todoText}</span><button onclick="deleteTodo(this)">삭제</button>`;
+  $completeList.appendChild($li);
+  $completeItem.remove();
   alert("해야 할 일이 완료되었습니다!");
 };
 
 const deleteTodo = (button) => {
-  const deleteLi = button.parentElement;
-  deleteLi.remove();
+  const $deleteLi = button.parentElement;
+  $deleteLi.remove();
   alert("한 일이 삭제되었습니다!");
 };
