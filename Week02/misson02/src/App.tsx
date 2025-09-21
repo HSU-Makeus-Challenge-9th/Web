947e21e57800b2cd77d436cd2efccdd77ef0b72f
hyunbin5921 <guieuna3@gmail.com>
2025-09-21 20:55:04 +0900

    feat: 2주차 미션 완료

---
+++ Week02/misson02/src/App.tsx
@@ -0,0 +1,13 @@
+
+import "./App.css";
+import ContextPage from "./useContext/context/ContextPage";
+
+function App() {
+  return (
+    <>
+      <ContextPage />
+    </>
+  );
+}
+
+export default App;
