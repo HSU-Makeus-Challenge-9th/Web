127d5d0be9caf5a87c025ec3d834c8c10205c2fe
임혜미 <imhyemi@imhyemiui-MacBookPro.local>
2025-10-07 22:48:57 +0900

    Remove mission1,2,3 from week04 root

--- Week04/mission1/src/components/movie/LodingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center h-64">
-      <div className="animate-spin rounded-full h-12 w-12 border-4 border-green-400 border-t-transparent"></div>
-    </div>
-  );
-}
--- Week04/mission2/src/pages/Signup.tsx
+++
@@ -1,6 +0,0 @@
-export default function Signup() {
-    return (
-      <div className="bg-gray-500 min-h-screen w-full"></div>
-    );
-  }
-  
