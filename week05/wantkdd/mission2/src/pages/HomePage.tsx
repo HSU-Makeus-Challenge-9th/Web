1746d076b053b2024f11bd40d8674d5c78132520
SangwanYu <ryusangwan12@gmail.com>
2025-10-26 12:45:53 +0900

    feat: 5주차 미션2 완료

---
+++ week05/wantkdd/mission2/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-8xl font-bold">WAN LP</h1>
+    </div>
+  );
+};
+
+export default HomePage;
