325aaee19db5337b6c2d35af91facdb763e926ca
Mgby1201sh <ajin1991@naver.com>
2025-11-08 23:10:01 +0900

    feat: 6주차 미션 완료

---
+++ week06/hyeok02/mission/src/hooks/usecreatecomment.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "../apis/axios";
+
+interface CreateCommentPayload {
+  lpId: number;
+  content: string;
+  order: 'asc' | 'desc';
+}
+
+export const useCreateComment = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({ lpId, content }: CreateCommentPayload) => {
+      const response = await axios.post(`/v1/lps/${lpId}/comments`, { content });
+      return response.data;
+    },
+    onSuccess: (_data, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ['lpComments', variables.lpId, variables.order],
+      });
+    },
+  });
+};
---
+++ week06/hyeok02/mission/src/hooks/uselikelp.ts
@@ -0,0 +1,11 @@
+import { useMutation } from "@tanstack/react-query";
+import axios from "../apis/axios";
+
+export const useLikeLp = (lpid: number) => {
+  return useMutation({
+    mutationFn: async () => {
+      const res = await axios.post(`/v1/lps/${lpid}/likes`);
+      return res.data;
+    },
+  });
+};
