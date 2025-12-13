528f8fead73cbb87c8ddfeca6e92fad19123f471
Gamgyul <ryusangwan12@gmail.com>
2025-12-14 01:29:00 +0900

    design: css누락분 추가

--- week09/wantkdd/Mission0/src/App.tsx
+++ week09/wantkdd/Mission0/src/App.tsx
@@ -1,10 +1,12 @@
-import UseReducerCompany from "./pages/useReducerCompany";
-// import UseReducerPage from "./pages/useReducerPage";
+// import UseReducerCompany from "./pages/useReducerCompany";
+import UseReducerPage from "./pages/useReducerPage";
+import "./app.css"
+
 
 function App() {
   return <>
-  {/* <UseReducerPage /> */}
-  <UseReducerCompany/>
+  <UseReducerPage />
+  {/* <UseReducerCompany/> */}
   </>;
 }
 
