b0e74b98785a58a6e5872d9889600f3f2535f768
Gamgyul <ryusangwan12@gmail.com>
2025-12-14 01:23:59 +0900

    feat: Creae Week9 Mission0

---
+++ week09/wantkdd/Mission0/src/App.tsx
@@ -0,0 +1,11 @@
+import UseReducerCompany from "./pages/useReducerCompany";
+// import UseReducerPage from "./pages/useReducerPage";
+
+function App() {
+  return <>
+  {/* <UseReducerPage /> */}
+  <UseReducerCompany/>
+  </>;
+}
+
+export default App;
---
+++ week09/wantkdd/Mission0/src/pages/useReducerPage.tsx
@@ -0,0 +1,101 @@
+import { useReducer, useState } from 'react'
+
+// 1. state에 대한 interface
+interface IState {
+  counter: number;
+  error: string | null;
+}
+
+// 2. reducer에 대한 interface
+interface IAction {
+  type: "INCREASE" | "DECREASE" | "RESET_TO_ZERO";
+  payload?: number;
+}
+
+function reducer(state: IState, action: IAction): IState {
+  const { type } = action;
+  console.log(action);
+  console.log(state);
+
+  switch (type) {
+    case "INCREASE": {
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    }
+    case "DECREASE": {
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    }
+    case "RESET_TO_ZERO": {
+      return {
+        ...state,
+        counter: 0,
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+export default function UseReducerPage() {
+  // 1. useState
+  const [count, setCount] = useState(0);
+
+  // 2. useReducer
+  const [state, dispatch] = useReducer(reducer, {
+    counter: 0,
+    error: null,
+  });
+
+  const handleIncrease = (): void => {
+    setCount(count + 1);
+  };
+
+  console.log(state);
+
+  return (
+    <div className="flex flex-col gap-10">
+      <div>
+        <h2 className="text-3xl">useState</h2>
+        <h2>useState 훅 사용: {count}</h2>
+        <button onClick={handleIncrease}>Increase</button>
+      </div>
+      <div>
+        <h2 className="text-3xl">useReducer</h2>
+        <h2 className="text-3xl">useReducer 훅 사용: {state.counter}</h2>
+        <button
+          onClick={(): void =>
+            dispatch({
+              type: "INCREASE",
+              payload: 3,
+            })
+          }
+        >
+          Increase
+        </button>
+        <button
+          onClick={(): void =>
+            dispatch({
+              type: "DECREASE",
+            })
+          }
+        >
+          DECREASE
+        </button>
+        <button
+          onClick={(): void =>
+            dispatch({
+              type: "RESET_TO_ZERO",
+            })
+          }
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
